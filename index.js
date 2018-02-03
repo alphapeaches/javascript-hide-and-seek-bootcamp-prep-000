e0266ccb98a3f2fa0616df474c42fa9bfe659f02
Rachel Acosta <denversunrise@gmail.com>
2018-02-03 02:11:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelectorAll('.target');
+}
+
+function deepestChild(){
+  querySelectorAll()
+}
+
+function increaseRankBy(n){
+  //.ranked-list
+}
